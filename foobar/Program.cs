dae4b063ce7316198bb2f152501cbd156f6ab7dc
beaumont117@gmail.com <beaumont117@gmail.com>
2018-04-02 17:33:09 -0500

    set up git

---
+++ foobar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace foobar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Beau!");
+        }
+    }
+}
