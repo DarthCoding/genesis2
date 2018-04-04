b004b2f5209553769a790775c00876a7dae6b928
beaumont117@gmail.com <beaumont117@gmail.com>
2018-04-03 21:20:19 -0500

    extras

--- foobar/Program.cs
+++ foobar/Program.cs
@@ -6,8 +6,12 @@ namespace foobar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Beau!");
+            var first = args.Length > 0 ? args[0] : String.Empty;
+            var second = args.Length > 1 ? args[1] : String.Empty;
+
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+
         }
     }
 }
