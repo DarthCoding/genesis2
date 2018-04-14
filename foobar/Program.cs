e0f3272f418c84f2eea56334b622998a3c1f68dc
beaumont117@gmail.com <beaumont117@gmail.com>
2018-04-14 12:45:20 -0500

    string manipulation

--- foobar/Program.cs
+++ foobar/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//using Dapper;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Transactions;
+using System.Configuration;
+using System.Collections.Generic;
 
 namespace foobar
 {
@@ -12,7 +19,60 @@ namespace foobar
 
             Console.WriteLine(first * second);
             
+            var dataPath = @"./Data";
+
+            Console.WriteLine($"These files are in {dataPath}");
+
+            var data = Directory.GetFiles(@"./Data");
+
+            foreach (var p in data) {
+
+                Console.WriteLine(p);
+                Console.WriteLine(File.ReadAllText(p));
+
+                var nums = File.ReadAllText(p);
+                var getNums = nums.Split();
+
+                Console.WriteLine(getNums[0]);
+
+                //var num1 = Convert.ToInt32(getNums[0]);
+                var num1 = int.Parse(getNums[0]);
+                var num2 = Convert.ToInt32(first);
+
+                Console.WriteLine(first + num1);
+
+            }
+
+
+
+
+            ReadData(data);
+
+
+
+
+
+
+
+
 
         }
+
+        public static void ReadData(string[] dataPoints) {
+
+            Console.WriteLine(dataPoints.Length);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
